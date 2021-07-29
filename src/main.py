3ee5dd9fb9a22579b517b8b4e7e82f9f07e195b6
Jasper Hawks <jasperhawks@gmail.com>
2021-07-28 20:41:05 -0400

    Continued work on the play function

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ import speech_recognition as sr # Library that allows us to find
 import pyttsx3  # Library that allows for text to speech
 import time # Library that allows us to manipulate time
 import requests # Library that allows us to send HTTP requests
-import re
+import re # Regex library for manipulating strings
+from selenium import webdriver
 from bs4 import BeautifulSoup # Library that allows us to scrape elements from an HTML file
 
 engine = pyttsx3.init() # Initialize the voice engine from pyttsx3
@@ -31,7 +32,7 @@ time.sleep(2)
 
 engine.say("I'm Jim how may I help you")
 
-# Run and wait allows Jim to pick up on sounds in the environment
+# Run and wait allows the voice assistant to wait for more commands to give it
 engine.runAndWait()
 
 def speak():
@@ -57,6 +58,7 @@ def speak():
     return(request)
 
 action = speak()
+
 print(action)
 
 if "play" in action:
@@ -64,21 +66,36 @@ if "play" in action:
     # We're going to have to get some sort of media player
     # I'm leaning towards vlc
 
+    driver = webdriver.Firefox()
+    # TODO check if play is empty or not
+
     playPattern = re.compile(".*Play.",re.IGNORECASE)
     search = re.sub(playPattern,"",action)
+    engine.say("Now playing: " +search)
     search = re.sub("\s","+",search)
 
-    req = requests.get("https://www.youtube.com/results?search_query=" + search).text
+#   req = requests.get("https://www.youtube.com/results?search_query=" + search,headers=headers).text
+#   print("https://www.youtube.com/results?search_query=" + search)
+
+    driver.get("https://www.youtube.com/results?search_query=" + search)
+
+    vid = driver.find_element_by_xpath("//*[@id=\"img\"]")
+    vid.click()
+
+    # TODO Implement a way to stop the video when it is done
+
 
     # Since Youtube doesn't have any structured way of making the URLS
     # of videos we'll have to take our first guess
 
     # If we want we can separate requests between music and videos and have Spotify
     # handle music while we deal with videos on Youtube
-    soup = BeautifulSoup(req,"html.parser")
+#   soup = BeautifulSoup(req,"html.parser")
+#   print(soup.prettify())
 
-    for link in soup.find_all(id_="thumbnail" and id.get("class")=="style-scope ytd-thumbnail no-transition"):
-        print(link)
+#   for link in soup.find_all("div", class_ = "style-scope ytd-section-list-renderer"):
+#       print("ran")
+#       print(link)
 
 
 
--- src/setup.py
+++ src/setup.py
@@ -1,2 +1,5 @@
 # This is the file that will run the setup before we run the main program. 
 # This has not been developed yet 
+
+# TODO DOWNLOAD THE SELENIUM DRIVER FOR THE USER
+# ALONG WITH OTHER PYTHON PACKAGES
